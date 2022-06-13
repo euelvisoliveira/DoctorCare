8d7a3cf3899b7313b00da2171184feaf54543b97
Elvis Ribeiro <elvis.ribeiro.oliveira@hotmail.com>
2022-06-12 21:25:03 -0300

    Proejeto-finalizado-2

--- main.js
+++ main.js
@@ -2,11 +2,43 @@ window.addEventListener('scroll', onScroll)
 
 onScroll()
 function onScroll() {
-  showNavOnScrollO()
-  showBackTopScroll()
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()
+
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
 }
 
-function showNavOnScrollO() {
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2
+
+  // verificar se a seção passou da linha
+  // quais dados vou precisar?
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+  const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+
+  // verificar se a base está abaixo da linha alvo
+
+  const sectionEndsAt = sectionTop + sectionHeight
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+
+  // limites da seção
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
+}
+
+function showNavOnScroll() {
   if (scrollY > 0) {
     navegar.classList.add('scroll')
   } else {
@@ -14,8 +46,8 @@ function showNavOnScrollO() {
   }
 }
 
-function showBackTopScroll() {
-  if (scrollY > 300) {
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 550) {
     backTop.classList.add('show')
   } else {
     backTop.classList.remove('show')
@@ -34,6 +66,13 @@ ScrollReveal({
   origin: 'top',
   distance: '30px',
   duration: 700
-}).reveal(
-  '#home, #home img, #home .stats, #services,#services header,#services .card, #about, #about header, #about .content'
-)
+}).reveal(`
+  #home, 
+  #home img, 
+  #home .stats, 
+  #services,
+  #services header,
+  #services .card
+  #about, 
+  #about header, 
+  #about .content`)
